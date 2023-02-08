5b76ff91e9bad85a48b60814410e1ce991edf437
Line Ploger <lilleploger@gmail.com>
2023-02-08 19:09:32 +0100

    kategori

--- js/category.js
+++ js/category.js
@@ -1,1 +1,20 @@
-//copy.querySelector(".read-more").setAttribute("href" `productlist.html?id=productlist`)
+fetch("https://kea-alt-del.dk/t7/api/categories")
+  .then((res) => res.json())
+  .then(showCategories);
+
+function showCategories(cats) {
+  cats.forEach(showCategory);
+}
+
+function showCategory(cat) {
+  //fanger vores template
+  const template = document.querySelector("template").content;
+  //cloner
+  const clone = template.cloneNode(true);
+  //ændre indhold
+  clone.querySelector("a").textContent = cat.category;
+  clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+
+  //appender
+  document.querySelector(".kategoriliste ul").appendChild(clone);
+}
